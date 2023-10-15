5a6ee0269b60ef7a4d92beca300db1bda852b1f3
Wookyung <namwookyung.dev@gmail.com>
2023-10-15 17:02:46 +0900

    import 및 스크립트 추가_23.10.15

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
-import '../src/styles/globals.css'
+import '../styles/css/styles.css'
+import type { AppProps } from 'next/app'
+import Script from 'next/script'
+import Header from '../components/header'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Header />
+      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"/>
+      <Component {...pageProps} />
+    </>
+  );
 }
-
-export default MyApp
