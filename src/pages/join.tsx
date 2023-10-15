979b6616ec9369ce8bcd001903437f03b2bfdeef
Wookyung <namwookyung.dev@gmail.com>
2023-10-15 17:04:10 +0900

    사용자 및 회원가입 파일 추가_23.10.15

---
+++ src/pages/join.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+
+export default function join() {
+  return (
+    <div className="bg-dark">
+      <div id="layoutAuthentication">
+        <div id="layoutAuthentication_content">
+          <main>
+            <div className="container">
+              <div className="row justify-content-center">
+                <div className="col-lg-5">
+                  <div className="card shadow-lg border-0 rounded-lg mt-5">
+                    <div className="card-header">
+                      <h3 className="text-center font-weight-light my-4">VARIETY</h3>
+                    </div>
+                    <div className="card-body">
+                      <form>
+                        <div className="form-floating mb-3">
+                          <input className="form-control" id="inputId" type="text" placeholder="아이디" />
+                          <label htmlFor="inputId">아이디</label>
+                        </div>
+                        <div className="form-floating mb-3">
+                          <input className="form-control" id="inputPassword" type="password" placeholder="Password" />
+                          <label htmlFor="inputPassword">비밀번호</label>
+                        </div>
+                        <Link className="btn btn-warning" href="/main">회원가입</Link>
+                      </form>
+                    </div>
+                    <div className="card-footer text-center py-3">
+                      <div className="small" style={{color:"gray"}}>Copyright &copy; Wookyung Nam 2023</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </main>
+        </div>
+      </div>
+    </div>
+  )
+}
