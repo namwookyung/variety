c8c6b34fecf56753053be35973560daa03d0cf3f
wookyung <wk.nam.dev@gmail.com>
2024-11-25 15:18:14 +0900

    메뉴 수정_24.11.25

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,4 +1,4 @@
-import { faAngleDown, faChalkboard, faChartLine, faList, faSignal, faTachometerAlt, faUserGear } from "@fortawesome/free-solid-svg-icons";
+import { faAngleDown, faBars, faBriefcase, faClapperboard, faCoins, faComments, faFaceSmile, faGamepad, faList, faListCheck, faPersonRunning, faUserGear } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 
@@ -8,31 +8,68 @@ export default function sidebar() {
             <nav className="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
                 <div className="sb-sidenav-menu">
                     <div className="nav">
-                        <div className="sb-sidenav-menu-heading">Main</div>
+                        {/* <div className="sb-sidenav-menu-heading">Main</div>
                         <Link className="nav-link" href="/main">
                             <div className="sb-nav-link-icon">
                                 <FontAwesomeIcon icon={faChalkboard} />
                             </div>
-                            Main Board
-                        </Link>
-                        <div className="sb-sidenav-menu-heading">Menu</div>
-                        <Link className="nav-link" href="/sensor/sensorList">
+                            Home
+                        </Link> */}
+                        <div className="sb-sidenav-menu-heading">Notice</div>
+                        <Link className="nav-link" href="/board">
                             <div className="sb-nav-link-icon">
-                                <FontAwesomeIcon icon={faTachometerAlt} />
+                                <FontAwesomeIcon icon={faList} />
                             </div>
-                            &nbsp;센서 제어
+                            공지사항
                         </Link>
-                        <Link className="nav-link" href="/relay/relayList">
+                        <div className="sb-sidenav-menu-heading">List</div>
+                        <Link className="nav-link" href="/board">
                             <div className="sb-nav-link-icon">
-                                <FontAwesomeIcon icon={faSignal} />
+                                <FontAwesomeIcon icon={faBars} />
                             </div>
-                            릴레이 제어
+                            전체
                         </Link>
-                        <Link className="nav-link" href="/graph/graphList">
+                        <Link className="nav-link" href="/board/life">
                             <div className="sb-nav-link-icon">
-                                <FontAwesomeIcon icon={faChartLine} />
+                                <FontAwesomeIcon icon={faComments} />
                             </div>
-                            &nbsp;센서 그래프
+                            일상/취미
+                        </Link>
+                        <Link className="nav-link" href="/board/work">
+                            <div className="sb-nav-link-icon">
+                                <FontAwesomeIcon icon={faBriefcase} />
+                            </div>
+                            취업/직장
+                        </Link>
+                        <Link className="nav-link" href="/board/game">
+                            <div className="sb-nav-link-icon">
+                                <FontAwesomeIcon icon={faGamepad} />
+                            </div>
+                            게임
+                        </Link>
+                        <Link className="nav-link" href="/board/enter">
+                            <div className="sb-nav-link-icon">
+                                <FontAwesomeIcon icon={faClapperboard} />
+                            </div>
+                            연예/방송
+                        </Link>
+                        <Link className="nav-link" href="/board/finance">
+                            <div className="sb-nav-link-icon">
+                                <FontAwesomeIcon icon={faCoins} />
+                            </div>
+                            금융
+                        </Link>
+                        <Link className="nav-link" href="/board/sports">
+                            <div className="sb-nav-link-icon">
+                                <FontAwesomeIcon icon={faPersonRunning} />
+                            </div>
+                            스포츠
+                        </Link>
+                        <Link className="nav-link" href="/board/etc">
+                            <div className="sb-nav-link-icon">
+                                <FontAwesomeIcon icon={faFaceSmile} />
+                            </div>
+                            기타
                         </Link>
                         <div className="sb-sidenav-menu-heading">Setting</div>
                         <a className="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseUsers" aria-expanded="false" aria-controls="collapseUsers">
@@ -46,16 +83,25 @@ export default function sidebar() {
                         </a>
                         <div className="collapse" id="collapseUsers" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                             <nav className="sb-sidenav-menu-nested nav">
-                                <Link className="nav-link" href="/user/userList">사용자 조회</Link>
-                                <Link className="nav-link" href="/user/userAdd">사용자 추가</Link>
+                                <Link className="nav-link" href="/user">사용자 조회</Link>
+                                <Link className="nav-link" href="/user/add">사용자 추가</Link>
                             </nav>
                         </div>
-                        <Link className="nav-link" href="/menu/menuList">
+                        <a className="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseUsers" aria-expanded="false" aria-controls="collapseUsers">
                             <div className="sb-nav-link-icon">
-                                <FontAwesomeIcon icon={faList} />
+                                <FontAwesomeIcon icon={faListCheck} />
                             </div>
-                            &nbsp;메뉴 관리
-                        </Link>
+                            공지사항 관리
+                            <div className="sb-sidenav-collapse-arrow">
+                                <FontAwesomeIcon icon={faAngleDown} />
+                            </div>
+                        </a>
+                        <div className="collapse" id="collapseUsers" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                            <nav className="sb-sidenav-menu-nested nav">
+                                <Link className="nav-link" href="/notice">공지사항 조회</Link>
+                                <Link className="nav-link" href="/notice/add">공지사항 추가</Link>
+                            </nav>
+                        </div>
                     </div>
                 </div>
             </nav>
